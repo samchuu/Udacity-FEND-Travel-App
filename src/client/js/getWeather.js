8e0068c06963ef4628691427af7baf0f45825980
Sam Chu <samchupiano@gmail.com>
2021-06-06 01:05:20 +0200

    refactor getWeather.js

--- src/client/js/getWeather.js
+++ src/client/js/getWeather.js
@@ -1,21 +1,12 @@
-
-const search = document.getElementById("zip");
-const searchValue = search.value;
-
-
 async function getGeoNames(input) {
     const geoUser = process.env.GEO_API_KEY;
     //make request to url
     const response = await fetch(`http://api.geonames.org/searchJSON?q=${input}&maxRows=1&username=${geoUser}`);
     const data = await response.json();
-    const latLng = {
-      lat:data.geonames[0].lat,
-      lng:data.geonames[0].lng
-       }
-  return getWeatherBit(latLng.lat, latLng.lng)
+    
+    return data
   }
 
-  
 async function getWeatherBit(lat, lng) {
   const wbUser = process.env.WEATHERBIT_KEY;
   //make request to url
@@ -38,13 +29,22 @@ async function getWeatherBit(lat, lng) {
   return weatherInfo;
 };
 
-
+const search = document.getElementById("zip");
 const button = document.getElementById("generate");
 button.addEventListener("click", () => {
-  getGeoNames(searchValue)
+  const currentVal = search.value;
+  console.log(getGeoNames(currentVal)); //get the whole data of geonames first then save in variable
+  const geoData = getGeoNames(currentVal);
+  console.log(geoData.then((data)=> {         //use .then to access the data received from the saved variable, then get specific data (lat lng)
+    const lat=data.geonames[0].lat;
+    const lng=data.geonames[0].lng;
+   console.log(getWeatherBit(lat,lng))
+  }))
+
+  })
  
  
-});
+;
 
 
 
